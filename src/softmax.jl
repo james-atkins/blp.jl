007976de05d961388ee07c2bf2b66ed01bc05931
James Atkins <hello@jamesatkins.net>
2022-03-04 15:48:15 +0000

    Generalise choice_probabilities
    Compute the sum of delta and mu on the fly without a temporary utility matrix.
    Make generic over AbstractFloat's.

--- src/softmax.jl
+++ src/softmax.jl
@@ -1,30 +1,37 @@
+export choice_probabilities!
 
-# Based on the SoftmaxThreePassReload function from
-# Dukhan and Ablavatski (2020), The Two-Pass Softmax Algorithm (https://arxiv.org/abs/2001.04438)
-function choice_probabilities(utilities::Matrix{Float64})
-    J, I = size(utilities)
-    choice_probs = Matrix{Float64}(undef, J, I)
+"""
+Compute the choice probabilities in-place using an overflow safe algorithm.
+
+Based on the SoftmaxThreePassReload function from
+Dukhan and Ablavatski (2020), The Two-Pass Softmax Algorithm (https://arxiv.org/abs/2001.04438).
+"""
+function choice_probabilities!(output::AbstractMatrix{T}, delta::AbstractVector{T}, mu::AbstractMatrix{T}) where T<:AbstractFloat
+    J, I = size(mu)
+    @assert length(delta) == J "delta and mu must have the same number of rows"
+    @assert size(output) == (J, I) "output matrix must be same shape as mu"
 
     @inbounds for i = 1:I
-        max_u = 0.0
+        max_u = T(0)
         for j = 1:J
-            if utilities[j, i] > max_u
-                max_u = utilities[j, i]
+            u = delta[j] + mu[j, i]
+            if u > max_u
+                max_u = u
             end
         end
 
         sigma = @fastmath exp(-max_u)
 
         @simd ivdep for j = 1:J
-            choice_probs[j, i] = @fastmath exp(utilities[j, i] - max_u)
-            sigma += choice_probs[j, i]
+            output[j, i] = @fastmath exp(delta[j] + mu[j, i] - max_u)
+            # SIMD makes left-to-right summation both faster and more accurate
+            # https://discourse.julialang.org/t/when-shouldnt-we-use-simd/18276/14
+            sigma += output[j, i]
         end
     
         sigma_inv = 1 / sigma
         @simd ivdep for j = 1:J
-            choice_probs[j, i] *= sigma_inv
+            output[j, i] *= sigma_inv
         end
     end
-
-    return choice_probs
 end
