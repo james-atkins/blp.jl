e73b864192be704e62934275a97973b26fcf3ba1
James Atkins <code@jamesatkins.net>
2023-11-26 18:02:03 -0600

    Add alpha to Theta2

--- src/theta2.jl
+++ src/theta2.jl
@@ -1,6 +1,17 @@
 struct Theta2{T <: AbstractFloat}
+    alpha::Union{T, Nothing}              # Linear price coefficient if not concentrated out
     sigma::LowerTriangular{T, Matrix{T}}  # K2 x K2 lower triangle
     pi::Matrix{T}                         # K2 x D
+
+    function Theta2(alpha::AbstractFloat, sigma::LowerTriangular, pi::Matrix)
+        T = promote_type(typeof(alpha), eltype(sigma), eltype(pi))
+
+        if size(sigma, 1) != size(sigma, 1)
+            throw(DimensionMismatch("sigma and pi must have the same number of rows"))
+        end
+
+        new{T}(alpha, sigma, pi)
+    end
 
     function Theta2(sigma::LowerTriangular, pi::Matrix)
         T = promote_type(eltype(sigma), eltype(pi))
@@ -9,7 +20,7 @@ struct Theta2{T <: AbstractFloat}
             throw(DimensionMismatch("sigma and pi must have the same number of rows"))
         end
 
-        new{T}(sigma, pi)
+        new{T}(nothing, sigma, pi)
     end
 end
 
@@ -20,7 +31,17 @@ end
 function Theta2(sigma::AbstractMatrix)
     K2 = size(sigma, 1)
     pi = Matrix{eltype(sigma)}(undef, K2, 0)
-    return Theta2(sigma, pi)
+    return Theta2(LowerTriangular(sigma), pi)
+end
+
+function Theta2(alpha::AbstractFloat, sigma::AbstractMatrix)
+    K2 = size(sigma, 1)
+    pi = Matrix{eltype(sigma)}(undef, K2, 0)
+    return Theta2(alpha, LowerTriangular(sigma), pi)
+end
+
+function Theta2(alpha::AbstractFloat, sigma::AbstractMatrix, pi::AbstractMatrix)
+    return Theta2(alpha, LowerTriangular(sigma), pi)
 end
 
 Base.eltype(::Type{Theta2{T}}) where {T} = T
@@ -29,6 +50,10 @@ function flatten(theta2::Theta2)
     K2, D = size(theta2.pi)
 
     n = div(K2 * (K2 + 1), 2) + (K2 * D)
+    is_alpha = theta2.alpha !== nothing
+    if is_alpha
+        n += 1
+    end
 
     sigma_idx_1 = 1
     sigma_idx_2 = div(K2 * (K2 + 1), 2)
@@ -55,8 +80,17 @@ function flatten(theta2::Theta2)
             vec(pi)[:] = x[pi_idx_1:pi_idx_2]
         end
 
-        return Theta2(sigma, pi)
+        if is_alpha
+            alpha = x[length(x)]
+            return Theta2(alpha, sigma, pi)
+        else
+            return Theta2(sigma, pi)
+        end
     end
 
-    return ([theta2.sigma[tril_indices(theta2.sigma)]; vec(theta2.pi)], unflatten)
+    if is_alpha
+        return ([theta2.sigma[tril_indices(theta2.sigma)]; vec(theta2.pi); theta2.alpha], unflatten)
+    else
+        return ([theta2.sigma[tril_indices(theta2.sigma)]; vec(theta2.pi)], unflatten)
+    end
 end
--- test/theta2.jl
+++ test/theta2.jl
@@ -1,4 +1,4 @@
-@testset "flatten/unflatten Theta2 without pi" begin
+@testset "flatten/unflatten Theta2 with sigma" begin
     K2 = 5
 
     sigma = randn(K2, K2)
@@ -11,7 +11,7 @@
     @test theta2.sigma == theta2_unflattened.sigma
 end
 
-@testset "flatten/unflatten Theta2" begin
+@testset "flatten/unflatten Theta2 with sigma, pi" begin
     K2, D = 4, 3
 
     sigma = randn(K2, K2)
@@ -22,6 +22,38 @@ end
 
     theta2_unflattened = unflatten(flat)
 
+    @test theta2.sigma == theta2_unflattened.sigma
+    @test theta2.pi == theta2_unflattened.pi
+end
+
+@testset "flatten/unflatten Theta2 with alpha, sigma" begin
+    K2 = 5
+
+    sigma = randn(K2, K2)
+    alpha = randn()
+
+    theta2 = Theta2(alpha, sigma)
+    flat, unflatten = flatten(theta2)
+
+    theta2_unflattened = unflatten(flat)
+
+    @test theta2.alpha == theta2_unflattened.alpha
+    @test theta2.sigma == theta2_unflattened.sigma
+end
+
+@testset "flatten/unflatten Theta2 with alpha, sigma, pi" begin
+    K2, D = 4, 3
+
+    sigma = randn(K2, K2)
+    pi = randn(K2, D)
+    alpha = randn()
+
+    theta2 = Theta2(alpha, sigma, pi)
+    flat, unflatten = flatten(theta2)
+
+    theta2_unflattened = unflatten(flat)
+
+    @test theta2.alpha == theta2_unflattened.alpha
     @test theta2.sigma == theta2_unflattened.sigma
     @test theta2.pi == theta2_unflattened.pi
 end
